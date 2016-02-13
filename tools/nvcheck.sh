c55937778a510d75fc1332f4cab2901c7ed64a9c
MURAOKA Taro <koron.kaoriya@gmail.com>
2016-02-14 02:17:42 +0900

    remove unused tools

--- tools/nvcheck.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-set -e
-
-find $1 -name "*.jax" -print | xargs nvcheck
--- tools/update-config.sh
+++
@@ -1,3 +0,0 @@
-GIT_REPO="git@github.com:vim-jp/vimdoc-ja.git"
-GIT_NAME="vimdoc-ja system"
-GIT_EMAIL="vimdoc-ja@vim-jp.org"
